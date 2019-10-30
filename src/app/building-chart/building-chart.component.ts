91471eb5c78beb51278b0fa14c1057ec52466018
z003xbfc <liyang.jiang@siemens.com>
2019-10-30 19:55:09 +0800

    Working on control functions of model

--- src/app/building-chart/building-chart.component.ts
+++ src/app/building-chart/building-chart.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { BuildingChartService } from './building-chart.service';
 import { zip as RxZip } from 'rxjs';
-import { Object3D } from 'three';
+import { Object3D, Mesh, Material } from 'three';
 
 @Component({
   selector: 'app-building-chart',
@@ -9,15 +9,22 @@ import { Object3D } from 'three';
   styleUrls: ['./building-chart.component.sass']
 })
 export class BuildingChartComponent implements OnInit {
-  floorKeys:string[];
-  @ViewChild('canvasContainer', {static: true}) canvasContainer: ElementRef;
+  floorKeys: string[];
+  floors: Mesh[];
+  @ViewChild('canvasContainer', { static: true }) canvasContainer: ElementRef;
 
   constructor(
     private compService: BuildingChartService
-  ) { }
+  ) {
+    this.floors = [];
+  }
 
   ngOnInit() {
     console.log('start to get information');
+
+    this.compService.objCasteredEvt.subscribe(obj => this.handleObjCastered.bind(this));
+    this.compService.objLeaveEvt.subscribe(obj => this.handleObjLeaved.bind(this));
+    this.compService.canvasClicked.subscribe(e => this.handleCanvasClicked.bind(this));
 
     RxZip(
       this.compService.getFloorsNames$(),
@@ -35,16 +42,38 @@ export class BuildingChartComponent implements OnInit {
     service.initRenderer(el);
     service.initCamera(el);
     service.initScene();
+    service.initLights();
     service.initGrid();
 
     service.loadModel$('building.dae')
-    .subscribe((model:any) => {
-      console.log('get model', model);
-      service.scene.add((model.scene) as Object3D);
-    }, err => {
-      console.error(err);
-      
-    });
+      .subscribe((model: any) => {
+        console.log('get model', model);
+        const modelScene = model.scene as Object3D;
+        // modelScene.traverse(child => console.log('child', child));
+        modelScene.traverse((child: any) => {
+          if (child.type === 'Mesh') {
+            child.castShadow = true;
+            child.receiveShadow = true;
+            this.floors.push(child);
+            // service.scene.add(child);
+          }
+        });
+        service.scene.add((modelScene));
+        service.play();
+      }, err => {
+        console.error(err);
+
+      });
+  }
+
+  handleObjCastered(obj) {
+    console.log('castered', obj);
+  }
+  
+  handleObjLeaved(obj) {}
+  
+  handleCanvasClicked(e:MouseEvent) {
+
   }
 
 }
--- src/app/building-chart/building-chart.service.ts
+++ src/app/building-chart/building-chart.service.ts
@@ -1,7 +1,19 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import {
-  WebGLRenderer, Scene, Color, PerspectiveCamera, GridHelper
+  WebGLRenderer,
+  Scene, Color,
+  PerspectiveCamera,
+  GridHelper,
+  Material,
+  AmbientLight,
+  HemisphereLight,
+  Fog,
+  DirectionalLight,
+  DirectionalLightHelper,
+  HemisphereLightHelper,
+  Raycaster,
+  Vector2, Mesh
 } from 'three';
 import {
   OrbitControls,
@@ -20,6 +32,18 @@ export class BuildingChartService {
   controls: OrbitControls;
   gridHelper: GridHelper;
 
+  mousePos: Vector2;
+  raycaster: Raycaster;
+
+  casteredObj: Mesh;
+
+  objCasteredEvt: EventEmitter<any>;
+  objLeaveEvt: EventEmitter<any>;
+  canvasClicked:EventEmitter<any>;
+
+  // returned by requestAnimationFrame
+  timer: number;
+
   private host = '/api';
 
   constructor(
@@ -34,11 +58,11 @@ export class BuildingChartService {
 
   getModel$() {
     const headers = new HttpHeaders({
-      'Accept': 'application/octet-stream',
+      Accept: 'application/octet-stream',
       'Content-Type': 'application/octet-stream'
     });
     return this.httpClient.get(`${this.host}/model`, {
-      headers: headers
+      headers
     });
   }
 
@@ -58,20 +82,22 @@ export class BuildingChartService {
   initRenderer(dom: HTMLElement) {
     const renderer = this.renderer;
     renderer.setSize(dom.clientWidth, dom.clientHeight);
-    renderer.shadowMap.enabled = true;// 打开辅助线
+    renderer.shadowMap.enabled = true;
+    renderer.domElement.style.display = 'block';
     dom.appendChild(renderer.domElement);
 
     dom.onresize = () => {
       renderer.setSize(dom.clientWidth, dom.clientHeight);
-      if(this.camera) {
+      if (this.camera) {
         this.camera.aspect = dom.clientWidth / dom.clientHeight;
         this.camera.updateProjectionMatrix();
       }
-    }
+    };
   }
 
   initScene() {
-    this.scene.background = new Color('#fff');
+    this.scene.background = new Color(0x22a32);
+    this.scene.fog = new Fog(0x191e24, 200, 1000);
   }
 
   initCamera(dom: HTMLElement) {
@@ -83,15 +109,101 @@ export class BuildingChartService {
     this.camera.position.set(pos.x, pos.y, pos.z);
     this.camera.lookAt(0, 0, 0);
     this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+    this.controls.target.set(0, 0, 0);
+
+    this.initRaycaster(this.renderer);
+  }
+
+  initLights() {
+    const hemiLight = new HemisphereLight(0xffffff, 0xffffff, .6);
+    hemiLight.color.setHSL(.6, 1, .6);
+    hemiLight.groundColor.setHSL(.095, 1, .75);
+    hemiLight.position.set(0, 50, 0);
+    this.scene.add(hemiLight);
+
+    const hemiLightHellper = new HemisphereLightHelper(hemiLight, 10);
+    this.scene.add(hemiLightHellper);
+
+    const dirLight = new DirectionalLight(0xffffff, 1);
+    dirLight.color.setHSL(0.1, 1, .95);
+    dirLight.position.set(0, 50, 0);
+    dirLight.castShadow = true;
+    dirLight.shadow.camera.top = 50;
+    dirLight.shadow.camera.bottom = -50;
+    dirLight.shadow.camera.left = -50;
+    dirLight.shadow.camera.right = 50;
+    this.scene.add(dirLight);
+
+    const dirLightHelper = new DirectionalLightHelper(dirLight, 10);
+    this.scene.add(dirLightHelper);
   }
 
   initGrid() {
-    this.gridHelper = new GridHelper(100, 50);
+    this.gridHelper = new GridHelper(2000, 20, 0xffffff, 0xaaaaff);
+    const mat = this.gridHelper.material as Material;
+    mat.opacity = .2;
+    mat.transparent = true;
     this.scene.add(this.gridHelper);
   }
 
+  play() {
+    this.renderScene();
+  }
+
+  pause() {
+    cancelAnimationFrame(this.timer);
+  }
+
+  renderScene() {
+    this.renderer.render(this.scene, this.camera);
+    this.controls.update();
+    this.renderRaycasterObj(this.raycaster, this.scene, this.camera, this.mousePos);
+    this.timer = requestAnimationFrame(this.renderScene.bind(this));
+  }
+
+  renderRaycasterObj(raycaster: Raycaster, scene: Scene, camera: PerspectiveCamera, mousePos: Vector2) {
+    raycaster.setFromCamera(mousePos, camera);
+    const intersects = raycaster.intersectObjects(scene.children);
+    if (intersects.length > 0) {
+      const currObj = intersects[0].object;
+      if ((currObj !== this.casteredObj) && (currObj instanceof Mesh)) {
+        this.casteredObj = currObj;
+        this.objCasteredEvt.emit(currObj);
+      }
+    } else {
+      this.objLeaveEvt.emit(this.casteredObj);
+      this.casteredObj = null;
+    }
+  }
+
+  private initRaycaster(renderer: WebGLRenderer) {
+    this.raycaster = new Raycaster();
+    this.mousePos = new Vector2();
+    const dom = renderer.domElement;
+    dom.addEventListener('mousemove', (e) => {
+      this.updateMousePos(e, dom);
+    });
+    dom.addEventListener('click', (e) => {
+      // this.updateMousePos(e, dom);
+      this.canvasClicked.emit(e);
+    });
+  }
+
+  private updateMousePos(e: MouseEvent, dom: HTMLCanvasElement) {
+    e.preventDefault();
+    const deltaX = e.clientX - dom.offsetLeft;
+    this.mousePos.setX((deltaX / dom.width) * 2 - 1);
+
+    const deltaY = e.clientX - dom.offsetLeft;
+    this.mousePos.setX((deltaY / dom.height) * 2 - 1);
+  }
+
   private initialize() {
-    this.renderer = new WebGLRenderer();
+    this.renderer = new WebGLRenderer({ antialias: true });
     this.scene = new Scene();
+
+    this.objCasteredEvt = new EventEmitter();
+    this.objLeaveEvt = new EventEmitter();
+    this.canvasClicked=new EventEmitter();
   }
 }
